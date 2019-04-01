7a03f1020007c2481d66957a97ee1c185045ac2c
Lena Korkeila <lkorkeila@github.com>
2019-04-01 16:54:03 -0700

    resolved root6 macro bug

--- example/ThreeDDemo_ROOT6.C
+++ example/ThreeDDemo_ROOT6.C
@@ -1,12 +1,18 @@
-R__ADD_INCLUDE_PATH(/home/onetrueteal/Programs/ANNIE/ANNIEEventDisplay/include/)
-R__ADD_LIBRARY_PATH(/home/onetrueteal/Programs/ANNIE/ANNIEEventDisplay/lib/)
-R__LOAD_LIBRARY(libEve.so)
-R__LOAD_LIBRARY(libEventDisplay.so)
+//R__ADD_INCLUDE_PATH(/home/lenak/ANNIE/ANNIEEventDisplay/include/)
+//R__ADD_LIBRARY_PATH(/home/lenak/ANNIE/ANNIEEventDisplay/lib/)
+//R__LOAD_LIBRARY(libEve.so)
+R__LOAD_LIBRARY(/home/lenak/ANNIE/ANNIEEventDisplay/lib/libEventDisplay.so)
 #include <vector>
 
 void ThreeDDemo_ROOT6(){
   // Load libraries
   // ==============
+  //gSystem->Load("libGeom");
+  //gSystem->Load("libEve");
+  //gSystem->Load("libMinuit");
+  //gSystem->Load("../lib/libEventDisplay.so");
+  //gSystem->AddIncludePath("-I/home/lenak/ANNIE/ANNIEEventDisplay/include/");
+  //gSystem->AddLinkedLibs("/home/lenak/ANNIE/ANNIEEventDisplay/lib/libEventDisplay.so");
   ANNIEDisplay* ad = new ANNIEDisplay();
   ad->SelectDataFile("PhaseIITestData.root");
   ad->LoadNTupleEvent();
