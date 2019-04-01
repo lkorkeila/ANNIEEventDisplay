be9a110a54081eb2811b786aafd0cf6a0581eaa2
Teal Pershing <tjpershing@ucdavis.edu>
2019-04-01 14:58:51 -0700

    Modified MakefileCling so it moves the rootmap and pcm to same directory as the shared object

---
+++ example/ThreeDDemo_PhaseII.C
@@ -0,0 +1,15 @@
+void ThreeDDemo_PhaseII(){
+  // Load libraries
+  // ==============
+  //gSystem->Load("libGeom");
+  gSystem->Load("libEve");
+  gSystem->Load("libMinuit");
+  gSystem->Load("./lib/libEventDisplay.so");
+  //gROOT->ProcessLine("#include <vector>");
+  ANNIEDisplay* ad = new ANNIEDisplay();
+  ad->SelectDataFile("PhaseIITest.root");
+  ad->LoadNTupleEvent();
+  ad->InitializeGUI();
+  ad->ShowEvent(); 
+}
+
